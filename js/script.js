dd77ab2807677411b927d4f4b63e50ad5124518f
Oleg <prodoter19911@mail.ru>
2021-07-13 17:30:11 +0300

    noviy2

--- js/script.js
+++ js/script.js
@@ -9,12 +9,24 @@ const personalMovieDB ={
     privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?',"");
-const b = prompt('На сколько оцените его?',"");
-const c = prompt('Один из последних просмотренных фильмов?',"");
-const d = prompt('На сколько оцените его?',"");
+for(let i = 0; i < 2; i++){
+    const a = prompt('Один из последних просмотренных фильмов?',"");
+    const b = prompt('На сколько оцените его?',"");
+    if(a == "" || b == "" || a.length == 50 || b.length == 50){
+        i--;
+        continue;
+    }
+    personalMovieDB.movies[a] = b;
+}
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+if(personalMovieDB.count < 10){
+    alert('Просмотрено довольно мало фильмов');
+}else if(10 < personalMovieDB.count < 30){
+    alert('Вы классический зритель');
+}else if(personalMovieDB > 30){
+    alert('Вы киноман');
+}else{
+    alert('Произошла ошибка');
+}
 
 console.log(personalMovieDB);
