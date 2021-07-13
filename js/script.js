24b756689cf4a682ab7f1b339d429b5c46a514d2
Oleg <prodoter19911@mail.ru>
2021-07-13 17:46:54 +0300

    noviy3

--- js/script.js
+++ js/script.js
@@ -12,18 +12,20 @@ const personalMovieDB ={
 for(let i = 0; i < 2; i++){
     const a = prompt('Один из последних просмотренных фильмов?',"");
     const b = prompt('На сколько оцените его?',"");
-    if(a == "" || b == "" || a.length == 50 || b.length == 50){
+    if(a != "" && b != "" && a != null && b != null && a.length < 50 ){
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else{
+        console.log("error");
         i--;
-        continue;
     }
-    personalMovieDB.movies[a] = b;
 }
 
 if(personalMovieDB.count < 10){
     alert('Просмотрено довольно мало фильмов');
-}else if(10 < personalMovieDB.count < 30){
+}else if(10 < personalMovieDB.count &&  personalMovieDB.count< 30){
     alert('Вы классический зритель');
-}else if(personalMovieDB > 30){
+}else if(personalMovieDB.count > 30){
     alert('Вы киноман');
 }else{
     alert('Произошла ошибка');
