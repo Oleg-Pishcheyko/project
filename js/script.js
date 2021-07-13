64893b621e60721a64ff1bc6b6c0e43cad56e02d
Oleg <prodoter19911@mail.ru>
2021-07-13 15:32:54 +0300

    niviy

--- js/script.js
+++ js/script.js
@@ -1,6 +1,20 @@
-"use strict";
+const numberOffilms = +prompt("Сколько фильмов вы уже посмотрели?","" );
 
 
-alert(5);
-const answer = prompt("Вам есть 18?","18");
-console.log(answer);
+const personalMovieDB ={
+    count: numberOffilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?',"");
+const b = prompt('На сколько оцените его?',"");
+const c = prompt('Один из последних просмотренных фильмов?',"");
+const d = prompt('На сколько оцените его?',"");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
---
+++ script.js
@@ -0,0 +1,5 @@
+'use strict';
+
+
+const numberOffilms = +prompt("Сколько фильмов вы уже посмотрели?","");
+console.log(numberOffilms);
